ff97009adfd2327835ebd1cbc6b8a3cf10d6a1fb
Dima Vasiliev <dumitru.v@goparrot.ai>
2020-09-20 18:20:35 +0300

    change url config

--- woo-config.js
+++ woo-config.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 axios.defaults.headers['Content-Type'] = 'application/json';
 
 const config = {
-  url: 'https://api.chicken.md',
+  url: 'https://chicken.vladimir-rodin.com/',
   consumerKey: 'ck_ee621c5433ba7db88bf08862a33f7cf5ceb7a3ff',
   consumerSecret: 'cs_4cfd576be1cd7eacb1d48602ef14897eb67983fd',
   version: 'wc/v3',
