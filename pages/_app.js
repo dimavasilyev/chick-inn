74b04d689a75b2a3d08509b6969449be662d7963
Dima Vasiliev <dumitru.v@goparrot.ai>
2021-04-04 11:21:54 +0300

    Added straus redirect

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import '../styles/index.css';
-
 import { CartProvider } from '../components/cart-provider/cart-provider';
 import { ProductsProvider } from '../components/products-provider/products-provider';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const strausUrl = 'https://www.straus.md/ru/restaurant/chickinn/';
+
+    window.location.replace(strausUrl);
+  }, []);
   return (
     <CartProvider>
       <ProductsProvider>
